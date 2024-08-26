6c33604f6f796c8b1c6fcb2794b4a5646bebe5bf
John Park <johnparkbluejay@gmail.com>
2024-08-26 16:15:00 -0400

    contact form styling complete - functionality still need work. 3d Earth model started

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  return (
-    <div>Earth</div>
-  )
-}
+	return <div>Earth</div>;
+};
 
-export default Earth
+export default Earth;
